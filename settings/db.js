fe86212bc0e4750a1585821b039be206365a179d
anastasiia-garlova <anastasiia.garalova@gmail.com>
2026-09-11 23:04:11 +0200

    first commit

---
+++ settings/db.js
@@ -0,0 +1,43 @@
+//const mysql = require("mysql");
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    port: 3306,
+    database: "playwords_db"
+})
+
+connection.connect( err => {
+    if (err) {
+        console.log(err);
+        return err;
+    }
+    else {
+        console.log("Connected!)");
+    }
+});
+
+//module.exports = connection;
+export {connection};
+
+/*
+let query = "SELECT * FROM users";
+
+connection.query(query, (err, result, fields) => {
+    console.log(err);
+    console.log(result);
+    console.log(result[1]['name']);
+    //console.log(fields);
+});
+
+connection.end( err => {
+    if (err) {
+        console.log(err);
+        return err;
+    }
+    else {
+        console.log("Closed!)");
+    }
+})*/
